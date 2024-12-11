2ef3f9fbfc917d9616e1166680b3ad25cde90d0d
Hao Liu <v2top1lyle@gmail.com>
2024-12-10 19:07:47 -0800

    feat: orphan

---
+++ data/author-info.ts
@@ -0,0 +1,31 @@
+export const AUTHOR_INFO = {
+  name: 'Thomas Hao Liu',
+  description:
+    'A personal space on the cloud where I document my programming journey, sharing lessons, insights, and resources for fellow developers.',
+  email: 'v2top1lyle@gmail.com',
+  identity: 'Learner | Builder',
+  address: {
+    city: 'North Vancouver, Canada',
+    flag: 'flag-Canada',
+    timeZone: -8,
+  },
+  work: {
+    company: '',
+    occupation: 'Senior iOS Developer',
+    location: 'North Vancouver, Canada',
+    website: '/',
+  },
+  social: {
+    github: 'https://github.com/lyleLH',
+    x: '/',
+    facebook: '/',
+    youtube: '/',
+    linkedin: '/',
+    threads: '/',
+    instagram: '/',
+    juejin: '/',
+  },
+  support: {
+    kofi: 'https://ko-fi.com/mengke',
+  },
+}
---
+++ data/site-metadata.ts
@@ -0,0 +1,50 @@
+export const SITE_METADATA = {
+  title: `HaoLiu's blog - HaoLiu's life journey`,
+  author: 'Hao Liu',
+  headerTitle: `HaoLiu's blog`,
+  description:
+    'A personal space on the cloud where I document my life journey, sharing lessons, insights, and resources for fellow developers.',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://www.imhaoliu.com',
+  siteLogo: `${process.env.BASE_PATH || ''}/static/images/profile.webp`,
+  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.jpeg`,
+
+  locale: 'en-US',
+  stickyNav: true,
+  analytics: {
+    umamiAnalytics: {
+      websiteId: process.env.NEXT_UMAMI_ID,
+      shareUrl: 'https://analytics.eu.umami.is/share/EqvpZYPABxaQA3mr/mengke.me',
+    },
+  },
+  newsletter: {
+    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehive
+    // Please add your .env file and modify it according to your selection
+    provider: 'buttondown',
+  },
+  comments: {
+    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    giscusConfig: {
+      // https://giscus.app/
+      repo: process.env.NEXT_PUBLIC_GISCUS_REPO!,
+      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID!,
+      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY!,
+      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID!,
+      mapping: 'title', // supported options: pathname, url, title
+      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+      metadata: '0',
+      theme: 'light',
+      darkTheme: 'transparent_dark',
+      themeURL: '',
+      lang: 'en',
+    },
+  },
+  search: {
+    provider: 'kbar',
+    kbarConfig: {
+      // path to load documents to search
+      searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`,
+    },
+  },
+}
